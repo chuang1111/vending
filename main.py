4ce63955205117ad1cdbff210300ec2095d8e7bb
Krakenstein1 <163864351+Krakenstein1@users.noreply.github.com>
2024-03-20 09:38:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,27 @@ def main():
   print("Welcome to the Vending Machine!")
   
   money = io.get_money()
-  print(money, invent.inventory_dict)
+  #display inventory_dict
+  print("Here is a list of items you can purchase:")
 
-index = 1
-for key, value in invent.inventory_dict.items():
-  print(f'{index}, {key} ({value[0]}) - ({value[1]}) left')
-  index += 1
+  index = 1
+  for key, value in invent.inventory_dict.items():
+    print(f'{index}, {key} ({value[0]}) - ({value[1]}) left')
+    index += 1
 
-key_list = list(invent.inventory_dict.items())
-value_list = list(invent.inventory_dict.values())
+  key_list = list(invent.inventory_dict.items())
+  value_list = list(invent.inventory_dict.values())
+  product = key_list[choice - 1]
+  price = value_list[choice -1][0]
+  print(f'You have selected a {product}. The price is ${price}.')
+
+
+  if money >= price:
+    change = money - price
+    print(f'You have inserted ${money}. Your change is ${change}.')
+
+
+  print(f'Thank you for your purchase! Enjoy your {product}!')
 
 if __name__ == "__main__":
   main()
